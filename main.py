a18a224fd261edcc7b6144e362471e92ad9f2a00
Mariusz Mazur <mmazur@redhat.com>
2025-05-15 16:39:32 +0200

    99% done

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 from typing import Any
-import httpx
 from mcp.server.fastmcp import FastMCP
+import subprocess
+
+async def run_command(args: list) -> str:
+    """Runs the command from provided arguments.
+
+    Args:
+        args: args
+
+    Returns:
+        A tuple containing (stdout_string, stderr_string)."""
+
+    try:
+        process = subprocess.run(args, capture_output=True, text=True, check=False)
+        stdout_string = process.stdout
+        stderr_string = process.stderr
+        return stdout_string, stderr_string
+    except FileNotFoundError:
+        return "", f"Error: command wasn't found, you're missing the onboarding step to install '{args[0]}'."
 
 # Initialize FastMCP server
 mcp = FastMCP("openshift-backplane")
 
-#@mcp.tool()
-#async def login_cluster(cluster_id: str) -> str:
-#    ""Log into an end cluster.
+@mcp.tool()
+async def cluster_login(cluster_id: str) -> str:
+    """Log into a cluster to be able to issues kubectl commands.
+    Use once per session unless connection is broken or user explicitly states they want to connect to a different cluster.
 
-#    Args:
-#        cluster_id: 
+    Args:
+        cluster_id: a string 32 or 36 characters long identifying a cluster."""
+
+    #(stdout, stderr) = await run_command(["ocm", "whoami"])
+    return "You are logged in"
 
 @mcp.tool()
 async def cluster_info(cluster_id: str) -> str:
